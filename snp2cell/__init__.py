cc034052145cf5bd5519c962ed599bb816b4fa91
Jan Patrick Pett <jpatrickpett@googlemail.com>
2023-10-09 13:34:35 +0100

    fix import

--- snp2cell/__init__.py
+++ snp2cell/__init__.py
@@ -1,7 +1,7 @@
+from snp2cell.snp2cell_class import SNP2CELL, NCPU
 from snp2cell import util
 from snp2cell import cli
 from snp2cell import recipes
-from snp2cell.snp2cell_class import SNP2CELL, NCPU
 
 __version__ = "0.1.0"
 __author__ = "J.P.Pett"
